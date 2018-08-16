4561449d1a0fa0f83fffeda8890a177977d82eb5
George Gomes <georgegomees@gmail.com>
2018-08-16 11:06:12 -0300

    Added project

---
+++ maBeer/maBeer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maBeer
+//
+//  Created by George Gomes on 16/08/18.
+//  Copyright © 2018 CrowCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
