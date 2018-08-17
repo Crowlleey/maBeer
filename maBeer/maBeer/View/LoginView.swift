bd17de6043f7011f1b3849328ba3e7a2346663c5
George Gomes <georgegomees@gmail.com>
2018-08-16 22:24:40 -0300

    update pods

--- maBeer/maBeer/View/LoginView.swift
+++ maBeer/maBeer/View/LoginView.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2018 CrowCode. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginView: UIView{
+    
+    var shouldSetupConstraints = true
+    
+    var tfEmail: UITextField!
+    var tfSenha: UITextField!
+    var btLogin: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // DE que maneira desenvolver uma aplicação que auxilie uma equipe de scrum
+        // a gerenciar suas tarefas por meio de um dispositivo movel a fim de
+        // aumentar a produtividade de trabalho.
+        
+        self.tfEmail = UITextField(frame: CGRect(x: 10, y: 10, width: 10, height: 10))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func updateConstraints() {
+        if(shouldSetupConstraints) {
+            // AutoLayout constraints
+            shouldSetupConstraints = false
+        }
+        super.updateConstraints()
+    }
+    // POD INSTALL
+}
