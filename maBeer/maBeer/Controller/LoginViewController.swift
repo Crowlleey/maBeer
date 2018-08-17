65344a2f0b05ecb84642f7649d6726a9bafb44e8
George Gomes <georgegomees@gmail.com>
2018-08-16 21:06:21 -0300

    Refactor architecture project

---
+++ maBeer/maBeer/Controller/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  maBeer
+//
+//  Created by George Gomes on 16/08/18.
+//  Copyright © 2018 CrowCode. All rights reserved.
+//
+
+import Foundation
---
+++ maBeer/maBeer/View/LoginView.swift
@@ -0,0 +1,9 @@
+//
+//  LoginView.swift
+//  maBeer
+//
+//  Created by George Gomes on 16/08/18.
+//  Copyright © 2018 CrowCode. All rights reserved.
+//
+
+import Foundation
