d9b3121830e31e334d540c90d8639d0074c045a5
George Gomes <georgegomees@gmail.com>
2018-08-18 14:01:23 -0300

    Add login button autolayout

--- maBeer/maBeer/Controller/LoginViewController.swift
+++ maBeer/maBeer/Controller/LoginViewController.swift
@@ -30,6 +30,9 @@ class LoginViewController: UIViewController {
         }
     }
     
+    func pressLogin(){
+        
+    }
     
     
 }
--- maBeer/maBeer/View/LoginView.swift
+++ maBeer/maBeer/View/LoginView.swift
@@ -13,11 +13,10 @@ class LoginView: UIView{
     
     var signInAction: (() -> Void)?
     
+    
     var shouldSetupConstraints = true
     
     let screenSize = UIScreen.main.bounds
-
-    var btLogin: UIButton!
     
     let tfEmail: UITextField = {
         let tf = UITextField()
@@ -32,7 +31,7 @@ class LoginView: UIView{
         return tf
     }()
     
-    let tfSenha: UITextField = {
+    let tfPassword: UITextField = {
         let tf = UITextField()
         tf.backgroundColor = .clear
         tf.layer.borderWidth = 1.0
@@ -46,9 +45,17 @@ class LoginView: UIView{
         return tf
     }()
 
+    let btLogin: UIButton = {
+        let bt = UIButton()
+        bt.setTitle("Login", for: .normal)
+        bt.backgroundColor = .green
+        return bt
+    }()
+    
     let btSignIn: UIButton = {
         let bt = UIButton()
-        
+        bt.setTitle("Login", for: .normal)
+        bt.backgroundColor = .green
         return bt
     }()
     
@@ -56,7 +63,8 @@ class LoginView: UIView{
         super.init(frame: frame)
         self.backgroundColor = .white
         addSubview(tfEmail)
-        addSubview(tfSenha)
+        addSubview(tfPassword)
+        addSubview(btLogin)
     }
     
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented")}
@@ -66,26 +74,63 @@ class LoginView: UIView{
             // AutoLayout constraints
             
             setupTextFields()
-            
-         
+            setupButtons()
             shouldSetupConstraints = false
         }
         super.updateConstraints()
     }
     
+// MARK: - Setup constraints
+    
     func setupTextFields(){
-        let edgesInset: CGFloat = 10.0
-        let topInset: CGFloat = 200.0
+    
+        tfEmail.translatesAutoresizingMaskIntoConstraints = false
         
-        tfEmail.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        // center horizontally and vertically
+        tfEmail.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        tfEmail.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        // make sure textfield width never exceeds the superview width
+        tfEmail.leftAnchor.constraint(greaterThanOrEqualTo: leftAnchor, constant: 20).isActive = true
+        tfEmail.rightAnchor.constraint(lessThanOrEqualTo: rightAnchor, constant: 20).isActive = true
+        
+        // give the textfield its default width, notice the lowered priority which
+        // allows this constraint to be broken when needed to satisfy the above constraints
+        let widthConstraint = tfEmail.widthAnchor.constraint(equalToConstant: 370)
+        widthConstraint.priority = UILayoutPriority.defaultHigh
+        widthConstraint.isActive = true
         tfEmail.heightAnchor.constraint(equalToConstant: 34).isActive = true
-        tfEmail.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        tfEmail.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-
-        tfSenha.widthAnchor.constraint(equalToConstant: 250).isActive = true
-        tfSenha.heightAnchor.constraint(equalToConstant: 34).isActive = true
-        tfSenha.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        tfSenha.topAnchor.constraint(equalTo: tfEmail.bottomAnchor, constant: 20).isActive = true
+        
+        tfPassword.translatesAutoresizingMaskIntoConstraints = false
+        
+        // center horizontally
+        tfPassword.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        tfPassword.topAnchor.constraint(equalTo: tfEmail.bottomAnchor, constant: 20).isActive = true
+        
+        // make sure textfield width never exceeds the superview width
+        tfPassword.leftAnchor.constraint(greaterThanOrEqualTo: leftAnchor, constant: 20).isActive = true
+        tfPassword.rightAnchor.constraint(lessThanOrEqualTo: rightAnchor, constant: 20).isActive = true
+        
+        // give the textfield its default width, notice the lowered priority which
+        // allows this constraint to be broken when needed to satisfy the above constraints
+        let widthConstraintP = tfPassword.widthAnchor.constraint(equalToConstant: 370)
+        widthConstraintP.priority = UILayoutPriority.defaultHigh
+        widthConstraintP.isActive = true
+        tfPassword.heightAnchor.constraint(equalToConstant: 34).isActive = true
     }
     
+    func setupButtons(){
+        btLogin.translatesAutoresizingMaskIntoConstraints = false
+        btLogin.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        btLogin.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20).isActive = true
+        
+        btLogin.leftAnchor.constraint(greaterThanOrEqualTo: leftAnchor, constant: 20).isActive = true
+        btLogin.rightAnchor.constraint(lessThanOrEqualTo: rightAnchor, constant: 20).isActive = true
+        
+        btLogin.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+        let widthConstraint = btLogin.widthAnchor.constraint(equalToConstant: 370)
+        widthConstraint.priority = UILayoutPriority.defaultHigh
+        widthConstraint.isActive = true
+    }
 }
